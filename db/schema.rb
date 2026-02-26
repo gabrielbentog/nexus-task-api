e31e77f25fd45f46e2993bb31b58c00a8c939c9c
gabrielbentog <bentogdev@gmail.com>
2026-02-26 10:13:30 -0300

    Feature: Add Portuguese translations to Devise and the last_activity_at column to the users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_12_142955) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_26_131227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -77,6 +77,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_12_142955) do
     t.jsonb "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_activity_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
