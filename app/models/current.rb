878706c2fe5fb5383b7791fcdf755f9ac2c3126c
gabrielbentog <bentogdev@gmail.com>
2026-02-26 09:40:04 -0300

    Refactor: Update the attributes of the Current class to include request_id and tenant.

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,3 +1,9 @@
 class Current < ActiveSupport::CurrentAttributes
-  attribute :user, :ip
+  # Liste tudo que gostaria de acessar em qualquer lugar do código
+  attribute :user, :request_id, :tenant
+
+  # Use callbacks se precisar normalizar/limpar algo
+  resets do
+    Time.zone = nil
+  end
 end
