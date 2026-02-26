dc34503a7dac15dc41615b0002f856cab0537785
gabrielbentog <bentogdev@gmail.com>
2026-02-26 16:57:54 -0300

    fix: cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "frontend.meudominio.com"
+    origins "*"
     resource "/api/*",
       headers: :any,
       methods: [:get, :post, :patch, :delete, :options],
